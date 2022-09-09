71a123a7ada39122260129ed4e1a80cf6eeef7c9
George Kravtsov <george_kravtsov@mail.ru>
2022-09-09 15:26:31 +0700

    09092022

--- Курсовая/Blanchard/js/script.js
+++ Курсовая/Blanchard/js/script.js
@@ -1,15 +1,14 @@
 let burger = document.querySelector('.burger');
-let menu = document.querySelector('.nav');
-
+let menu = document.querySelector('.nav-wrapper');
 let menuLinks = menu.querySelectorAll('.nav__item');
+// let btnLogin = document.querySelector('.header__login-btn');
 
 burger.addEventListener('click',
 
   function () {
     burger.classList.toggle('burger--active');
-
     menu.classList.toggle('nav--active');
-
+    // btnLogin.classList.toggle('header__login-btn--active');
     document.body.classList.toggle('stop-scroll');
   })
 
@@ -24,3 +23,29 @@ menuLinks.forEach(function (elem) {
 
   })
 })
+
+
+///////////////////////////////////////////////
+// swiper
+///////////////////////////////////////////////
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  // direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
