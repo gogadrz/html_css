058347010ed7aa72012dc4509f5fc2da17e48a14
George Kravtsov <george_kravtsov@mail.ru>
2022-09-12 02:56:56 +0700

    structure changed

---
+++ Diplom/Blanchard/js/script.js
@@ -0,0 +1,77 @@
+let burger = document.querySelector('.burger');
+let menu = document.querySelector('.nav-wrapper');
+let menuLinks = menu.querySelectorAll('.nav__item');
+// let btnLogin = document.querySelector('.hero__btn');
+
+// burger click
+burger.addEventListener('click',
+  function () {
+    burger.classList.toggle('burger--active');
+    menu.classList.toggle('nav--active');
+    document.body.classList.toggle('stop-scroll');
+  })
+
+// login button click -- Не работает!
+// btnLogin.addEventListener('click',
+//   function () {
+//     burger.classList.remove('burger--active');
+//     menu.classList.remove('nav--active');
+//     document.body.classList.remove('stop-scroll');
+//     console.log('btn click!..');
+//   }
+// )
+
+
+// menu click
+menuLinks.forEach(function (elem) {
+  elem.addEventListener('click', function () {
+    burger.classList.remove('burger--active');
+    menu.classList.remove('nav--active');
+    document.body.classList.remove('stop-scroll');
+  })
+})
+
+///////////////////////////////////////////////
+// swiper
+///////////////////////////////////////////////
+const swiper = new Swiper('.hero__swiper', {
+  loop: true,
+  speed: 300,
+  allowTouchMove: true,
+});
+
+
+// set .hero height
+let heroBlock = document.querySelector('.hero');
+let headerHeight = document.querySelector('.header').clientHeight;
+let swiperHeight = document.querySelector('.swiper').clientHeight;
+
+heroBlock.style.height = swiperHeight + 'px';
+
+// gallery
+// select
+const element = document.querySelector('.gallery__select');
+
+const choices = new Choices(element, {
+  searchEnabled: false,
+  shouldSort: false,
+  itemSelectText: '',
+  allowHTML: false
+});
+
+
+// gallery swiper
+const gallerySwiper = new Swiper('.gallery__swiper', {
+  loop: true,
+
+  pagination: {
+    el: '.gallery__swiper-pagination',
+    type: "fraction",
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.gallery__swiper-button-next',
+    prevEl: '.gallery__swiper-button-prev',
+  },
+
+});
