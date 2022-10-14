90cc23b45f12fc0b76fa7195fba8cb5d11cd130c
George Kravtsov <george_kravtsov@mail.ru>
2022-10-14 18:59:56 +0700

    mud changed

--- mud/script.js
+++ mud/script.js
@@ -1,5 +1,29 @@
 (() => {
-  document.addEventListener('DOMContentLoaded', () => {    
+  document.addEventListener('DOMContentLoaded', () => {
+
+    const getDomainsBtn = document.querySelector('.btn');
+    // console.log(getDomainsBtn);
+
+    getDomainsBtn.addEventListener('click', () => {
+      const links = getPageLinksDomains();
+      console.log(links);
+    });
+
+    function getPageLinksDomains() {
+      return Array.from(document.getElementsByTagName('a'))
+      .map(link => link.href
+        .replace('http://', '')
+        .replace('https://', '')
+        .replace('www.', '')
+        .split('/')
+        .shift()
+        )
+
+        .reduce((uniqueDomains, domain) => {
+          if (uniqueDomains.includes(domain)) return uniqueDomains;
+          return [...uniqueDomains, domain];
+        }, []);
+    }
 
   });
 })();
